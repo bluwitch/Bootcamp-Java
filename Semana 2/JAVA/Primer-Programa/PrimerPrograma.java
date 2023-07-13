de3d57a124c7868b330ddd7684e45e96ee7d1b82
Martina Reta <martuh8v@gmail.com>
2023-07-13 20:19:47 -0300

    a

---
+++ Semana 2/JAVA/Primer-Programa/PrimerPrograma.java
@@ -0,0 +1,11 @@
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        String[] codingDojo = new String[3];
+        codingDojo[0] = "Mi nombre es Coding Dojo";
+        codingDojo[1] = "Tengo 100 años de edad.";
+        codingDojo[2] = "Mi ciudad es Burbank, CA";
+        System.out.println(codingDojo[0]);
+        System.out.println(codingDojo[1]);
+        System.out.println(codingDojo[2]);
+    }
+}
