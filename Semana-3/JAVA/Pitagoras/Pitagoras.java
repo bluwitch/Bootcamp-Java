129c5ad77d7c37742c48e45fcd34aabc507b950f
Martina Reta <martuh8v@gmail.com>
2023-07-14 00:03:48 -0300

    a

---
+++ Semana-3/JAVA/Pitagoras/Pitagoras.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+public class Pitagoras {
+    public double calcularHipotenusa(int catetoA, int catetoB) {
+        int i = catetoA;
+        int j = catetoB;
+        int hipotenusaResult = Math.sqrt(i * i + j * j);
+    }
+}
