e2af33e1d82c58645e94695a41976092a679da69
Martina Reta <martuh8v@gmail.com>
2023-07-14 00:55:51 -0300

    a

---
+++ Semana-3/JAVA/Pitagoras/TeoremaPitagoras.java
@@ -0,0 +1,7 @@
+public class TeoremaPitagoras {
+    public static void main(String[] args) {
+        Pitagoras pitagoras = new Pitagoras();
+        pitagoras.calcularHipotenusa(0, 0);
+        System.out.println(pitagoras.calcularHipotenusa(3, 3));
+    }
+}
