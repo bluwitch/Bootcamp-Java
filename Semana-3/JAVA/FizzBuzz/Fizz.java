18986720169ccc8c0f48bfbe0cbbd08022eb30bf
Martina Reta <martuh8v@gmail.com>
2023-07-14 02:36:14 -0300

    a

---
+++ Semana-3/JAVA/FizzBuzz/Fizz.java
@@ -0,0 +1,19 @@
+public class Fizz {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+    }
+
+    public static String fizzBuzz(int numero) {
+            if (numero % 3 == 0 && numero % 5 == 0) {
+                return "FizzBuzz";
+            } else if (numero % 3 == 0) {
+                return "Fizz";
+            } else if (numero % 5 == 0) {
+                return "Buzz";
+            } else {
+            return String.valueOf(numero);
+        }
+    }
+}
