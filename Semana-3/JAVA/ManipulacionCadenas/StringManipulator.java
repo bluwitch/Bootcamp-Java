abbaf3e87944b3a4dfac70e42de0ba7a9500bfc2
Martina Reta <martuh8v@gmail.com>
2023-07-17 19:37:47 -0300

    -m dsjdaj

--- Semana-3/JAVA/ManipulacionCadenas/StringManipulator.java
+++ Semana-3/JAVA/ManipulacionCadenas/StringManipulator.java
@@ -6,4 +6,18 @@ public class StringManipulator {
         String concatenatedString = trimStr1 + " " + trimStr2; // puse un espacio para que quede mas lindo el string
         return concatenatedString;
     }
+
+    public static Integer getIndexOrNull(String string, char letter) {
+        Integer index = string.indexOf(letter);
+        if (index == -1) {
+            return null;
+        } else {
+            return index;
+        }
+    }
+
+    public static Integer getIndexSub(String word, String subString) {
+        int index = word.indexOf(subString);
+        return index;
+    }
 }
--- Semana-3/JAVA/ManipulacionCadenas/StringManipulatorTesting.java
+++ Semana-3/JAVA/ManipulacionCadenas/StringManipulatorTesting.java
@@ -1,7 +1,26 @@
-
 public class StringManipulatorTesting {
     public static void main(String[] args) {
-        String str = StringManipulator.manipulator("  hola  ", "  diablo  ");
+        StringManipulator manipulator = new StringManipulator();
+
+        String str = manipulator.manipulator("  hola  ", "  diablo  ");
         System.out.println(str);
+
+        char letter = 'n';
+        Integer a = manipulator.getIndexOrNull("Coding", letter);
+        Integer b = manipulator.getIndexOrNull("Hola Mundo", letter);
+        Integer c = manipulator.getIndexOrNull("Saludar", letter);
+        System.out.println(a); // 4
+        System.out.println(b); // 7
+        System.out.println(c); // -1
+
+        String word = "Hola";
+        String subString = "la";
+        String notSubString = "mundo";
+
+        Integer d = manipulator.getIndexSub(word, subString);
+        Integer e = manipulator.getIndexSub(word, notSubString);
+
+        System.out.println(d); // 2
+        System.out.println(e); // -1
     }
 }
