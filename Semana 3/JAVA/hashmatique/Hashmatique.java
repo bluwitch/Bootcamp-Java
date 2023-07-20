71e285f5c04f7d846d5dd560b40c1e39111e0e62
Martina Reta <martuh8v@gmail.com>
2023-07-20 19:00:01 -0300

    a

--- Semana 3/JAVA/hashmatique/Hashmatique.java
+++ Semana 3/JAVA/hashmatique/Hashmatique.java
@@ -2,8 +2,13 @@ import java.util.HashMap;
 
 public class Hashmatique {
 
-    public static void main(String args){
+    public static void main(String[] args){
         HashMap<String, String> tracklist = new HashMap<String, String>();
-        tracklist.set
+        tracklist.put("Canción A ",  " Letra A... Lorem ipsum dolor sit amet...");
+        tracklist.put("Canción B ",  " Letra B...consectetur adipiscing elit...");
+        tracklist.put("Canción C ",  " Letra C...sed do eiusmod tempor incididunt...");
+        tracklist.put("Canción D ",  " Letra D... ut labore et dolore magna aliqua...");
+
+        System.out.println(tracklist);
     }
 }
