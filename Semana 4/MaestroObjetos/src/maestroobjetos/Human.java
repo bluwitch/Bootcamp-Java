ea73b9c68274a79ce17ceee8ead19f796c6c8f9b
Martina Reta <martuh8v@gmail.com>
2023-07-24 03:19:55 -0300

    a

--- Semana 4/MaestroObjetos/src/maestroobjetos/Human.java
+++ Semana 4/MaestroObjetos/src/maestroobjetos/Human.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package maestroobjetos;
 
-/**
- *
- * @author Martu
- */
 public class Human {
-    
+
+    private int health;
+    private int stealth;
+    private int intelligence;
+    private int strength;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStealth() {
+        return stealth;
+    }
+
+    public void setStealth(int stealth) {
+        this.stealth = stealth;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public Human() {
+
+    }
+
 }
