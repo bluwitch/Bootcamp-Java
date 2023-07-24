8a3e6a95643e53c3f1f45589e4dc226b408c54a1
Martina Reta <martuh8v@gmail.com>
2023-07-24 05:57:45 -0300

    a

--- Semana 4/MaestroObjetos/src/maestroobjetos/Human.java
+++ Semana 4/MaestroObjetos/src/maestroobjetos/Human.java
@@ -3,9 +3,9 @@ package maestroobjetos;
 public class Human {
 
     private int health = 100;
-    private int stealth = 3;
-    private int intelligence = 3;
-    private int strength = 3;
+    private int stealth;
+    private int intelligence;
+    private int strength;
     private String rol;
 
     public int getHealth() {
