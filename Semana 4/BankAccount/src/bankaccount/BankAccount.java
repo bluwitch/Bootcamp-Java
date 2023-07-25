b909331418c8fe708cf5e33dd16792194925d8a7
Martina Reta <martuh8v@gmail.com>
2023-07-25 16:36:14 -0300

    !

--- Semana 4/BankAccount/src/bankaccount/BankAccount.java
+++ Semana 4/BankAccount/src/bankaccount/BankAccount.java
@@ -1,6 +1,15 @@
-
 package bankaccount;
 
 public class BankAccount {
-    
+
+    String numeroCuenta;
+    Double SueldoCorriente;
+    Double SueldoAhorro;
+    public static int cantidadCuentas = 0;
+    public static double rastrearDinero = 0;
+
+    private static int[] numeroDeCuenta() {
+        
+        return;
+    }
 }
