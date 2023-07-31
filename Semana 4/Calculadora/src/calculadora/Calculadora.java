b6259b82d6db0bd8a6d468ad2781085a3ae52303
Martina Reta <martuh8v@gmail.com>
2023-07-31 11:04:33 -0300

    a

--- Semana 4/Calculadora/src/calculadora/Calculadora.java
+++ Semana 4/Calculadora/src/calculadora/Calculadora.java
@@ -3,7 +3,13 @@ package calculadora;
 public class Calculadora {
 
     public static void main(String[] args) {
-       
+        Operaciones operacion = new Operaciones();
+        operacion.setOperand1(10);
+        operacion.setOperand2(9);
+        operacion.setOperation("+");
+        String resultado = operacion.performOperation();
+        System.out.println("El resultado es: " + resultado);
+
     }
 
 }
--- Semana 4/Calculadora/src/calculadora/Operaciones.java
+++ Semana 4/Calculadora/src/calculadora/Operaciones.java
@@ -1,8 +1,52 @@
-
 package calculadora;
 
 // @author Martu
-
 public class Operaciones {
-    
+
+    private int operand1;
+    private int operand2;
+    private String operation;
+    private int result;
+
+    public int getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(int operand1) {
+        this.operand1 = operand1;
+    }
+
+    public int getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(int operand2) {
+        this.operand2 = operand2;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public String performOperation() {
+        if (operation.equals("+")) {
+            result = operand1 + operand2;
+        } else if (operation.equals("-")) {
+            result = operand1 - operand2;
+        }
+        return String.valueOf(result);
+    }
+
 }
