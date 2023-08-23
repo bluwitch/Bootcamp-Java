c0d51a09959151496e6d988a6c74f7f949a07a93
Martina Reta <martuh8v@gmail.com>
2023-08-23 06:34:32 -0300

    1

--- Semana 5/Languages/src/main/java/com/bootcamp/languages/services/LanguageService.java
+++ Semana 5/Languages/src/main/java/com/bootcamp/languages/services/LanguageService.java
@@ -2,33 +2,30 @@ package com.bootcamp.languages.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.stereotype.Service;
-
 import com.bootcamp.languages.models.Language;
 import com.bootcamp.languages.repositories.LanguageRepo;
-
-import jakarta.validation.Valid;
+import jakarta.transaction.Transactional;
 
 @Service
 public class LanguageService {
 	private final LanguageRepo languageRepo;
 
 	public LanguageService(LanguageRepo languageRepo) {
-	        this.languageRepo = languageRepo;
-	    }
+		this.languageRepo = languageRepo;
+	}
 
-	// Devolviendo todos los libros.
+	// Devuelve todos los lenguajes
 	public List<Language> allLanguages() {
 		return languageRepo.findAll();
 	}
 
-	// Creando un libro.
+	// Crea un lenguaje
 	public Language createBook(Language b) {
 		return languageRepo.save(b);
 	}
 
-	// Obteniendo la información de un libro
+	// Obtiene la información de un lenguaje
 	public Language findLanguage(Long id) {
 		Optional<Language> optionalLanguage = languageRepo.findById(id);
 		if (optionalLanguage.isPresent()) {
@@ -38,8 +35,17 @@ public class LanguageService {
 		}
 	}
 
-	public void updateLanguage(@Valid Language language) {
-		// TODO Auto-generated method stub
-		
+	@Transactional
+	public void updateLanguage(Language language) {
+        // Verificar si el lenguaje ya existe en la base de datos
+		Language existingLanguage = languageRepo.findById(language.getId()).orElse(null);
+		if (existingLanguage != null) {
+			// Actualiza los campos necesarios del lenguaje existente
+			existingLanguage.setName(language.getName());
+			existingLanguage.setCreator(language.getCreator());
+			existingLanguage.setCurrentVersion(language.getCurrentVersion());
+			// Guardar los cambios en la base de datos
+			languageRepo.save(existingLanguage);
+		}
 	}
 }
