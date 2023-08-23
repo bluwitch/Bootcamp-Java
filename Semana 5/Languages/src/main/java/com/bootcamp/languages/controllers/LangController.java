a79a9ec7deb6ebaeea680711ebfe5c89c8e4bccd
Martina Reta <martuh8v@gmail.com>
2023-08-23 19:32:36 -0300

    a

--- Semana 5/Languages/src/main/java/com/bootcamp/languages/controllers/LangController.java
+++ Semana 5/Languages/src/main/java/com/bootcamp/languages/controllers/LangController.java
@@ -3,26 +3,36 @@ package com.bootcamp.languages.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.bootcamp.languages.models.Language;
 import com.bootcamp.languages.repositories.LanguageRepo;
 
-@RestController
+@Controller
 public class LangController {
-	
+
 	@Autowired
 	private LanguageRepo repo;
 
-	@GetMapping
-	public List<Language> listaLenguajes(){
-		return repo.findAll();
+	@GetMapping("/languages")
+	public String showLanguages(Model model) {
+		// Obtener la lista de lenguajes desde el repositorio
+		List<Language> languages = repo.findAll();
+
+		// Agregar la lista de lenguajes al modelo
+		model.addAttribute("languages", languages);
+
+		// Devolver el nombre de la vista HTML
+		return "languages";
 	}
 	
-	@PostMapping
-	public void insertarLenguaje(Language lang){
-		repo.save(lang);
-	}
+	
+
+//	@PostMapping
+//	public void insertarLenguaje(Language lang) {
+//		repo.save(lang);
+//	}
 }
